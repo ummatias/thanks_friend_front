7c6516a34ae692b24e21bba8855ed4670116cbee
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2022-10-15 10:17:18 -0300

    fix: remove logged prop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout/layout'
-import { AuthProvider } from '../contexts/AuthContext'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout/layout";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <ChakraProvider>
-        <Layout logged={false}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
