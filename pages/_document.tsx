98ae74059ce9f72d8630c50636c8e72c4df8290d
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2022-10-18 00:02:49 -0300

    fix: page title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,41 +1,41 @@
-import { NextPage } from 'next'
-import Document, { Main, NextScript } from 'next/document'
-import Head from 'next/head'
-import { ServerStyleSheet } from 'styled-components'
+import { NextPage } from "next";
+import Document, { Main, NextScript } from "next/document";
+import Head from "next/head";
+import React from "react";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: NextPage) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: NextPage) => (props: any) =>
+          sheet.collectStyles(<App {...props} />),
+        enchanceComponent: (Component: React.Component) => Component,
+      });
 
-      const initialProps = await Document.getInitialProps(ctx)
-      
-      return {
-        ...initialProps,
-        styles: (
-          <html>
-            <Head>
-              <title>Thanks Friend</title>
-              <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;0,900;1,900&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
-            </Head>
-            <body>
-              {initialProps.styles}
-              {sheet.getStyleElement()}
-              <Main />
-              <NextScript />
-            </body>
-          </html>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <html>
+          <Head>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;0,900;1,900&family=Roboto:wght@400;700&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
+          <body>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+            <Main />
+            <NextScript />
+          </body>
+        </html>
+      ),
+    };
   }
 }
