45221e69c6c232828a1e1a05a5dc81c206d203c2
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2022-10-18 00:16:02 -0300

    fix: wave

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,14 +1,22 @@
-import { Image } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Image
-      position={"absolute"}
-      minW={"100%"}
-      bottom={0}
-      src="/wave.svg"
-      alt="logo"
-    />
+    <Box
+      display="block"
+      w={"100vw"}
+      zIndex={-1}
+      position="absolute"
+      overflow={"hidden"}
+    >
+      <Image
+        position={"absolute"}
+        minW={"100%"}
+        bottom={0}
+        src="/wave.svg"
+        alt="logo"
+      />
+    </Box>
   );
 };
 
